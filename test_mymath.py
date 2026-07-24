6fd5d811d2bf161ceca2512665c5aa89aad84162
K-m-q <khalid33086759@gmail.com>
2026-07-24 14:10:13 +0300

    Add tests for add and is_even functions

--- test_mymath.py
+++ test_mymath.py
@@ -2,3 +2,15 @@
 from mymath import add
 
 assert add(2, 3) == 5
+# test_mymath.py
+from mymath import add, is_even
+
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+
+
+def test_is_even():
+    assert is_even(4) is True
+    assert is_even(7) is False
